e3bf1e96babcd4c67c1947bb246f55ebe5f2029e
David Calhoun <david.b.calhoun@gmail.com>
2021-06-20 18:26:40 -0400

    initial checkin of hugo/public

---
+++ hugo/public/wp-content/uploads/2019/year-in-photos/resize.js
@@ -0,0 +1,78 @@
+import sharp from 'sharp';
+import fs from 'fs';
+import util from 'util';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+    
+const readFile = util.promisify(fs.readFile);
+const readDir = util.promisify(fs.readdir);
+
+const OUTPUT_SIZES = [500, 1000, 2000];
+const OUTPUT_FORMAT_OPTIONS = [
+    {
+        format: 'jpeg',
+        options: {
+           quality: 80,
+           progressive: true,
+           optimizeScans: true
+        }
+    },
+    {
+        format: 'webp',
+        options: {
+           quality: 80,
+           reductionEffort: 6
+        }
+    }
+];
+
+function getStuff() {
+  return readFile('test');
+}
+
+async function processFilesInDir(dir) {
+    let file;
+    try {
+        file = await readFile('20190112-5D__1419-stanford-theatre-palo-alto-apu-trilogy.jpg');
+    } catch(e) {
+        console.error(e);
+    }
+
+    let files;
+    try {
+        files = await readDir(dir);
+    } catch(e) {
+        console.error(e);
+    }
+
+    files.forEach(function (file) {
+        const isJPEG = file.endsWith('.jpg') || file.endsWith('.jpeg');
+        const isAlreadyResized = file.endsWith('-resize.jpg') || file.endsWith('-resize.jpeg');
+
+        if (isJPEG && !isAlreadyResized) {
+            const [name] = file.split('.jpg');
+
+            OUTPUT_SIZES.forEach(size => {
+                OUTPUT_FORMAT_OPTIONS.forEach(({ format, options }) => {
+                    sharp(file)
+                        .resize(size)
+                        [format](options)
+                        .toFile(`${name}-${size}px-resize.${format}`, (err, info) => {
+                            if (err) {
+                                console.error(`Problem processing ${file}`);
+                                console.error(err);
+                            }
+                        });
+                });
+            });
+
+        }
+    });
+}
+
+
+processFilesInDir(__dirname);
+
